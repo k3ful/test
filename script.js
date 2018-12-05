540e8e41209affd349d161a12c16711cb40b0455
Ivan <example@gmail.com>
2018-12-05 16:52:41 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+console.log(4/0)
+console.log('string'*9)
+
+let something;
+console.log(something)
+
+let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0])
+
+// alert('Hello, World!');
+
+// let answer = confirm("Are u here?");
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', 'Да');
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
